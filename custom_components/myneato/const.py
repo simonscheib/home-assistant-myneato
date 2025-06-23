ddebb678fb76bdaaa5571ed9e7a8339d33c418af
simonscheib <simon@blogteil.de>
2025-06-23 14:29:41 +0200

    Update const.py

--- custom_components/myneato/const.py
+++ custom_components/myneato/const.py
@@ -12,7 +12,7 @@ MYNEATO_FLOORPLANS = "myneato_floorplans"
 
 SCAN_INTERVAL_MINUTES = 1
 
-MODE = {1: "Eco", 2: "Turbo", 3: "Max"}
+MODE = {1: "Eco", 2: "Turbo", 3: "Max", 4: "Auto"}
 
 ACTION = {}
 
